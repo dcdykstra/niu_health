8390c61a324c1dc5a78d208214641c191f23f147
Danah Dykstra <70775448+dcdykstra@users.noreply.github.com>
2024-07-18 19:43:32 -0400

    upload files to drive

---
+++ v2/src/load/gdriveupload.py
@@ -0,0 +1,17 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+from src.config.configlog import config, logger
+
+
+def upload_files(files: list):
+    gauth = GoogleAuth()
+    drive = GoogleDrive(gauth)
+
+    for upload_file in files:
+        name = upload_file.split("/")[-1]
+        gfile = drive.CreateFile(
+            {"title": name, "parents": [{"id": config.drivefolderid}]}
+        )  # Read file and set it as the content of this instance.
+        gfile.SetContentFile(upload_file)
+        gfile.Upload()  # Upload the file.
