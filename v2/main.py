4d70dfb9f157bbfd024a44deb565111b9363269a
Danah Dykstra <70775448+dcdykstra@users.noreply.github.com>
2024-07-18 21:38:11 -0400

    main entrypoint

---
+++ v2/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import datetime as dt
+
+from src.scraper.scrape import scrape_yesterday, scrape_date
+from src.transform.transform import (
+    parse_appointments,
+    parse_cpts,
+    parse_visitors,
+    merge_daily_report,
+)
+from src.load.gdriveupload import upload_files
+
+if __name__ == "__main__":
+    # date = scrape_date("2024-07-13")
+    date = scrape_yesterday()
+    apt_df = parse_appointments()
+    cpt_df = parse_cpts()
+    vis_df = parse_visitors()
+
+    final = merge_daily_report(apt_df, cpt_df, vis_df)
+    final.to_excel(f"data/output/detainee_list_{date}.xlsx", index=False)
+
+    upload_files([f"data/output/detainee_list_{date}.xlsx"])
